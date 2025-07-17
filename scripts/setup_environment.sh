17b261d7f9b615a65e2d39eb219b81d4ca7113d1
CybergNetwork <89824700+CybergNetwork@users.noreply.github.com>
2025-07-17 14:34:18 -0600

    Update setup_environment.sh

--- scripts/setup_environment.sh
+++ scripts/setup_environment.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+pip install transformers torch accelerate
+pip install flash-attn --no-build-isolation  # For Flash Attention 2
+pip install langchain langchain-community
+pip install tree-sitter tree-sitter-python tree-sitter-javascript
+pip install gitpython pyparsing
+pip install fastapi uvicorn websockets
+pip install huggingface_hub
+huggingface-cli login
